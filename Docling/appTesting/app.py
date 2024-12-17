f19f1eb359d45cd873dd4333342216b65bf08db6
Shubhankar Abhay Kamthankar <80084594+So-bonkers@users.noreply.github.com>
2024-12-17 21:28:44 +0530

    don't know wtf this version is supposed to be doing

--- Docling/appTesting/app.py
+++ Docling/appTesting/app.py
@@ -7,7 +7,7 @@ import logging
 import time
 from werkzeug.utils import secure_filename
 from docling.datamodel.pipeline_options import PdfPipelineOptions
-from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.document_converter import DocumentConverter, PdfFormatOption, ConversionStatus
 from docling.datamodel.base_models import FigureElement, InputFormat, Table
 from docling_core.types.doc import PictureItem, TableItem
 
