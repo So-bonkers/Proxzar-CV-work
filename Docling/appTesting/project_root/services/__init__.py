451c3e3f7605550675c3d514f7d52123f3f7a3c7
Shubhankar Abhay Kamthankar <shubhankar.k@research.iiit.ac.in>
2024-12-20 15:11:08 +0530

    Remove obsolete PDF and HTML table files from ingested data

--- Docling/appTesting/project_root/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .document_processor import process_document
-from .utils import generate_client_id, save_temp_file, validate_file_format, move_file_to_output
-__all__ = [
-    "process_document",
-    "save_temp_file",
-    "validate_file_format",
-    "generate_client_id",
-    "move_file_to_output",
-]
