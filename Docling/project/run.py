11595f9e41566fb8dedfb6657bfb0c44e6f60da1
Shubhankar Abhay Kamthankar <shubhankar.k@research.iiit.ac.in>
2025-01-17 17:36:05 +0530

    cleanup the trash

--- Docling/project/run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Create the Flask application
-app = create_app()
-
-if __name__ == "__main__":
-    # Run the Flask application in debug mode
-    app.run(debug=True)
